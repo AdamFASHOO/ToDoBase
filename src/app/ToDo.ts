44cad628f6b67db61d78dd51043005c1fb6d0ba7
AdamFASHOO <AdamChapman1994@gmail.com>
2022-07-14 16:20:46 -0400

    base ToDo

---
+++ src/app/ToDo.ts
@@ -0,0 +1,9 @@
+export class ToDo{
+    TaskName: string;
+    Completed: boolean;
+
+    constructor(TaskName: string, Completed: boolean){
+        this.TaskName = TaskName;
+        this.Completed = Completed;
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDo } from './ToDo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoList';
+  ToDoList: ToDo[] = [
+    new ToDo("Water the cat", true),
+    new ToDo("Take out the trash", false),
+    new ToDo("Steal someone's homework", false),
+    new ToDo("Steal neighbors lawn gnome", true),
+    new ToDo("Tell people the truth about birds", false)
+  ];
+  newToDo: ToDo = new ToDo("", false);
+  filterText: string = "";
+  filteredTasks: ToDo[] = this.ToDoList;
+
+  addTask(): void{
+    let t = Object.assign({}, this.newToDo);
+    this.ToDoList.push(t);
+  }
+
+  completeTask(index: number): void {
+    let t: ToDo = this.ToDoList[index];
+    t.Completed = true;
+  }
+
+  removeTask(index: number): void {
+    this.ToDoList.splice(index, 1);
+  }
+
+  filterArray(tName: string) {
+    this.filteredTasks = this.ToDoList.filter(t => t.TaskName.includes(tName));
+  }
 }
